6172739c2b073e62ed1da5f9ba55df22748a15a2
Miroslav Jelaska <jelaska.miro@gmail.com>
2015-04-30 00:54:23 +0200

    Added some other algorithms

---
+++ other/CalculateExpressionTree.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace ConsoleApplication2
+{
+    abstract class Node
+    {
+        public Node Left  { get; set; }
+        public Node Right { get; set; }
+    }
+    class OperationNode : Node
+    {
+        public OperationNode(Func<int, int, int> operation)
+        {
+            this.Operation = operation;
+        }
+        private readonly Func<int, int, int> Operation;
+
+        public int Calculate(int a, int b)
+        {
+            return Operation(a, b);
+        }
+    }
+
+    class OperandNode : Node
+    {
+        public OperandNode(int value)
+        {
+            this.Value = value;
+        }
+        public int Value { get; private set; }
+    }
+
+    abstract class Algorithms
+    { 
+        public static int CalculateExpressionTree(Node node)
+        {
+            if (node is OperandNode)
+            {
+                return (node as OperandNode).Value;
+            }
+
+            return 
+                (node as OperationNode)
+                .Calculate(
+                    CalculateExpressionTree(node.Left),
+                    CalculateExpressionTree(node.Right)
+                );
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var root = new OperationNode((a, b) => a + b)
+            {
+                Left = new OperationNode((a, b) => a * b)
+                {
+                    Left = new OperandNode(3),
+                    Right = new OperationNode((a,b) => a - b)
+                    {
+                        Left = new OperandNode(1),
+                        Right = new OperandNode(2)
+                    }
+                },
+                Right = new OperandNode(4),
+            };
+
+            System.Console.WriteLine(Algorithms.CalculateExpressionTree(root));
+
+            System.Threading.Thread.Sleep(3000);
+        }
+    }
+}
---
+++ other/LevelOrder.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+namespace ConsoleApplication2
+{
+    class Node<T>
+    {
+        public Node(int value)
+        {
+            this.Value = value;
+        }
+
+        public int     Value { get; private set; }
+        public Node<T> Left  { get; set; }
+        public Node<T> Right { get; set; }
+
+        public override string ToString()
+        {
+            return this.Value.ToString(CultureInfo.InvariantCulture);
+        }
+    }
+    class BinaryTree <T>
+    {
+        private Queue<Node<T>> NodesForPrint = new Queue<Node<T>>();
+        public void PrintLevelOrder(Node<T> node)
+        {
+            System.Console.WriteLine(node);
+            
+            AddChildrenForPrint(node, NodesForPrint);
+            
+            var nextNodesForPrint = new Queue<Node<T>>();
+            while(NodesForPrint.Any())
+            {
+                while (NodesForPrint.Any())
+                {
+                    var currentNode = NodesForPrint.Dequeue();
+                    System.Console.WriteLine(currentNode);
+                    AddChildrenForPrint(currentNode, nextNodesForPrint);
+                }
+                NodesForPrint = nextNodesForPrint;
+            }
+        }
+        void AddChildrenForPrint(Node<T> node, Queue<Node<T>> queue)
+        {
+            if (node.Left != null)
+            {
+                queue.Enqueue(node.Left);
+            }
+            if (node.Right != null)
+            {
+                queue.Enqueue(node.Right);
+            }
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n1 = new Node<int>(1);
+
+            var n2 = new Node<int>(2);
+            var n3 = new Node<int>(3);
+            
+            var n4 = new Node<int>(4);
+            var n5 = new Node<int>(5);
+            var n6 = new Node<int>(6);
+
+            n1.Left = n2;
+            n1.Right = n3;
+
+            n2.Left = n4;
+            n3.Left = n5;
+            n3.Right = n6;
+
+            var binTree = new BinaryTree<int>();
+            binTree.PrintLevelOrder(n1);
+
+            System.Threading.Thread.Sleep(3000);
+        }
+    }
+}
